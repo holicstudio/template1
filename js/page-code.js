b3645a2951aef8c7f8d589398c24f7b24ce54970
holicstudio <admin@holicdev.com>
2015-08-12 15:19:24 -0500

    First hit

---
+++ js/page-code.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+   
+    //--- Image back
+    $('.image').replaceWith(function(i, v){
+        return $('<figure/>', {
+            style: 'background-image: url('+this.src+')',
+        }) 
+    });
+    
+    //--- fix placeholder
+			$(function() {
+				var input = document.createElement("input");
+					if(('placeholder' in input)==false) { 
+						$('[placeholder]').focus(function() {
+							var i = $(this);
+							if(i.val() == i.attr('placeholder')) {
+								i.val('').removeClass('placeholder');
+								if(i.hasClass('password')) {
+									i.removeClass('password');
+									this.type='password';
+								}			
+							}
+						}).blur(function() {
+							var i = $(this);	
+							if(i.val() == '' || i.val() == i.attr('placeholder')) {
+								if(this.type=='password') {
+									i.addClass('password');
+									this.type='text';
+								}
+								i.addClass('placeholder').val(i.attr('placeholder'));
+							}
+						}).blur().parents('form').submit(function() {
+							$(this).find('[placeholder]').each(function() {
+								var i = $(this);
+								if(i.val() == i.attr('placeholder'))
+									i.val('');
+							})
+						});
+				}
+		});
+});
